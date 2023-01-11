f57cdd1dc2d74b00feef4c51fd390e1f8af284b2
eclipse7723 <eclipse7723@gmail.com>
2023-01-11 20:45:09 +0100

    add app and postgresql

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'supervisor'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DATABASE_NAME", "db"),
+        'USER': os.getenv("DATABASE_USERNAME", "root"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD", "root"),
+        'HOST': os.getenv("DATABASE_HOST", "127.0.0.1"),
+        'PORT': os.getenv("DATABASE_PORT", "5432"),
     }
 }
 
